81f1a28bcdc189c1971c77c4c6dce00ec53a5929
Aman Singh <amansingh@amans-mbp.lan>
2024-02-23 18:03:08 -0800

    Adjusted app.py def()

--- application/app.py
+++ application/app.py
@@ -10,24 +10,24 @@ def home():
     return render_template('home.html')
 
 @app.route('/about/amandeepsingh')
-def about():
+def about_amandeepsingh():
     return render_template('about_amandeep.html')
 
 # @app.route('/about/aymanearfaoui')
-# def about():
+# def about_aymanearfaoui():
 #     return render_template('about_aymanearfaoui.html')
 
 # @app.route('/about/alexisalvarez')
-# def about():
+# def about_alexisalvarez():
 #     return render_template('about_alexisalvarez.html')
 
 # @app.route('/about/davedaly')
-# def about():
+# def about_davedaly():
 #     return render_template('about_davedaly.html')
 
-# @app.route('/about/markusreyer')
-# def about():
-#     return render_template('about_markusreyer.html')
+@app.route('/about/markusreyer')
+def about_markusreyer():
+    return render_template('about_markusreyer.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
